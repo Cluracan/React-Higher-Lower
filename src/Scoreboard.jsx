b882301e1fbb82c274d50c81341fe39a81697c8e
cluracan1 <cluracan1@gmail.com>
2025-02-19 21:39:26 +0000

    remove animationActive

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,15 +1,9 @@
-export default function Scoreboard({
-  score,
-  highScore,
-  cardsRemaining,
-  animationActive,
-}) {
+export default function Scoreboard({ score, highScore, cardsRemaining }) {
   return (
     <div className="scoreboard">
       <p>Current Score: {score}</p>
       <p>High score: {highScore}</p>
       <p>Cards Remaining: {cardsRemaining}</p>
-      <p>Animation Active: {animationActive ? "True" : "False"}</p>
     </div>
   );
 }
